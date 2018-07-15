9e2ad0f10391c39a4bd5bc282d3046d1d0f6368d
Roy Hong <sroyhong313@gmail.com>
2018-07-15 17:17:22 +0900

    Render OAuth Login Buttons

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,24 +2,66 @@ import React from 'react'
 
 import { images } from '../common/ImageUtils'
 
-const facebookBtn = {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'flex-start',
+const styles = {
+  facebookBorder: {
+    width: 1,
+    height: '100%',
+    marginLeft: 17,
+    backgroundColor: 'white'
+  },
+  facebookBtn: {
     width: 230,
     height: 40,
+    flex: 1,
     borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
     backgroundColor: '#507cc0'
-}
-
-const kakaoBtn = {
-  flex: 1,
-  width: 230,
-  height: 40,
-  borderRadius: 20,
-  backgroundColor: '#ffcd00',
-  alignItems: 'center',
-  justifyContent: 'flex-start'
+  },
+  facebookLogo: {
+    width: 12,
+    height: 21,
+    marginLeft: 12
+  },
+  kakaoBorder: {
+    height: '100%',
+    width: 1,
+    marginLeft: 9,
+    backgroundColor: 'white'
+  },
+  kakaoBtn: {
+    width: 230,
+    height: 40,
+    flex: 1,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    backgroundColor: '#ffcd00'
+  },
+  kakaoLogo: {
+    width: 23,
+    height: 22,
+    marginLeft: 9,
+    marginTop: 5,
+  },
+  loginContainer: {
+    paddingTop: 20
+  },
+  loginFacebook: {
+    fontSize: 14,
+    marginLeft: 14,
+    color: 'white',
+    fontWeight: 'bold'
+  },
+  loginKakao: {
+    fontSize: 14,
+    marginLeft: 14,
+    color: 'white',
+    fontWeight: 'bold'
+  },
+  optionDivider: {
+    paddingTop: 4
+  }
 }
 
 export default class Login extends React.Component {
@@ -29,28 +71,28 @@ export default class Login extends React.Component {
         <div className="level-item">
           <img src={images.logo} alt='logo' />
         </div>
-        <div className="level-item" style={{ paddingTop: 20 }}>
+        <div className="level-item" style={styles.loginContainer}>
           <div>
-            <a className="button" style={facebookBtn}>
+            <a className="button" style={styles.facebookBtn}>
               <div>
-                <img style={{ marginLeft: 12, width: 12, height: 21 }} src={images.fbLogo} alt='fb_logo' />
+                <img style={styles.facebookLogo} src={images.fbLogo} alt='fb_logo' />
               </div>
-              <div style={{ marginLeft: 17, height: '100%', width: 1, backgroundColor: 'white' }} />
-              <p style={{ fontSize: 14, marginLeft: 14, color: 'white', fontWeight: 'bold'}}>
+              <div style= {styles.facebookBorder} />
+              <p style={styles.loginFacebook}>
                 페이스북 계정으로 로그인
               </p>
             </a>
           </div>
         </div>
 
-        <div className="level-item" style={{ paddingTop: 4 }}>
+        <div className="level-item" style={styles.optionDivider}>
           <div>
-            <a className="button" style={kakaoBtn}>
+            <a className="button" style={styles.kakaoBtn}>
               <div>
-                <img style={{ marginLeft: 9, marginTop: 5, width: 23, height: 22 }} src={images.ktLogo} alt='kt_logo' />
+                <img style={styles.kakaoLogo} src={images.ktLogo} alt='kt_logo' />
               </div>
-              <div style={{ marginLeft: 9, height: '100%', width: 1, backgroundColor: 'white' }} />
-              <p style={{ fontSize: 14, marginLeft: 14, color: 'white', fontWeight: 'bold'}}>
+              <div style={styles.kakaoBorder} />
+              <p style={styles.loginKakao}>
                 카카오 계정으로 로그인
               </p>
             </a>

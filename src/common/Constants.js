a17bcdd8556e323a5462953d6d7159be12cf7a74
Roy Hong <sroyhong313@gmail.com>
2018-07-29 14:25:50 +0900

    Render Ranking Page

--- src/common/Constants.js
+++ src/common/Constants.js
@@ -2,5 +2,7 @@ export const COLORS = {
   SEA_BLUE: '#507cc0',
   WARM_GREY: 'rgb(155, 155, 155)',
   WHITE: '#ffffff',
-  YELLOW: '#ffcd00'
+  YELLOW: '#ffcd00',
+  GREEN: '#228B22',
+  LIGHT_GREY: '#f2f2f2'
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,102 @@
 import React from 'react'
 
+import { COLORS } from '../common/Constants'
 import { images } from '../common/ImageUtils'
+
+// TODO(royhong): Clean up CSS, avoid inline styling
+// TODO(royhong): Make Contents responsive
+const styles = {
+  bookmarkContainer: {
+    paddingLeft: 10
+  },
+  bookmarkContents: {
+    flexDirection: 'row',
+    width: 25,
+    height: 25,
+    borderRadius: 12,
+    backgroundColor: 'transparent'
+  },
+  currentLoves: {
+    marginLeft: 5,
+    display: 'inline',
+    fontSize: 10
+  },
+  greenBar: {
+    width: 56,
+    height: 4,
+    marginTop: -4,
+    backgroundColor: COLORS.GREEN,
+    zIndex: 2
+  },
+  loveIcon: {
+    justifyContent: 'center'
+  },
+  loveContainer: {
+    width: 114,
+    height: 25,
+    borderRadius: 12,
+    backgroundColor: 'transparent',
+    justifyContent: 'center'
+  },
+  loveContainerEven: {
+    width: 114,
+    height: 25,
+    borderRadius: 12,
+    backgroundColor: 'transparent',
+    justifyContent: 'center',
+    marginLeft: 16
+  },
+  outerContainerEven: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingTop: 7
+  },
+  outerContainerOdd: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    paddingTop: 7
+  },
+  sandwichContainer: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  sandwichInfoContainer: {
+    width: 341,
+    height: 100,
+    borderRadius: 4,
+    backgroundColor: COLORS.LIGHT_GREY
+  },
+  sandwichInfoOuterContainer: {
+    flexDirection: 'row',
+    marginTop: -25,
+    justifyContent: 'flex-end'
+  },
+  sandwichTitle: {
+    width: '80%',
+    alignItems: 'flex-end',
+    fontSize: 26,
+    fontWeight: 'bold',
+    marginLeft: 14,
+    textAlign: 'right',
+    marginRight: 14,
+    zIndex: 1
+  },
+  shareContainer: {
+    paddingLeft: 7,
+    paddingRight: 17
+  },
+  shareContents: {
+    flexDirection: 'row',
+    width: 25,
+    height: 25,
+    borderRadius: 12,
+    backgroundColor: 'transparent'
+  }
+}
 
 class SearchBar extends React.PureComponent {
   render () {
@@ -17,64 +113,118 @@ class SearchBar extends React.PureComponent {
   }
 }
 
-class StatusBar extends React.Component {
+class StatusBar extends React.PureComponent {
   constructor (props) {
     super(props)
     this.state = {
-      numLikes: 256
+      numLoves: 256
     }
   }
 
+  // TODO(royhong): Switch second bookmark to export icon
   render () {
-    return (
-      <div className='is-flex' style={{ flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center', paddingTop: 7 }}>
-        <div>
-          <button className='is-flex' style={{ width: 114, height: 25, borderRadius: 12, backgroundColor: 'transparent', justifyContent: 'center' }}>
-            <img src={images.love} style={{ justifyContent: 'center' }} alt='love' />
-            <p style={{ marginLeft: 5, display: 'inline', fontSize: 10 }}> {this.state.numLikes}명이 좋아해요</p>
-          </button>
+    if (this.props.id % 2 === 1) {
+      return (
+        <div className='is-flex' style={styles.outerContainerOdd}>
+          <div>
+            <button className='is-flex' style={styles.loveContainer}>
+              <img src={images.love} style={styles.loveIcon} alt='love' />
+              <p style={styles.currentLoves}> {this.state.numLoves}명이 좋아해요</p>
+            </button>
+          </div>
+          <div style={styles.bookmarkContainer}>
+            <button className='is-flex' style={styles.bookmarkContents}>
+              <i className='far fa-bookmark' />
+            </button>
+          </div>
+          <div style={styles.shareContainer}>
+            <button className='is-flex' style={styles.shareContents}>
+              <i className='far fa-bookmark' />
+            </button>
+          </div>
         </div>
-        <div style={{ paddingLeft: 10 }}>
-          <button className='is-flex' style={{ flexDirection: 'row', width: 25, height: 25, borderRadius: 12, backgroundColor: 'transparent' }}>
-            <i className='far fa-bookmark' />
-          </button>
+      )
+    } else {
+      return (
+        <div className='is-flex' style={styles.outerContainerEven}>
+          <div>
+            <button className='is-flex' style={styles.loveContainerEven}>
+              <img src={images.love} style={styles.loveIcon} alt='love' />
+              <p style={styles.currentLoves}> {this.state.numLoves}명이 좋아해요</p>
+            </button>
+          </div>
+          <div style={styles.bookmarkContainer}>
+            <button className='is-flex' style={styles.bookmarkContents}>
+              <i className='far fa-bookmark' />
+            </button>
+          </div>
+          <div style={styles.shareContainer}>
+            <button className='is-flex' style={styles.shareContents}>
+              <i className='far fa-bookmark' />
+            </button>
+          </div>
         </div>
-        <div style={{ paddingLeft: 7, paddingRight: 17 }}>
-          <button className='is-flex' style={{ flexDirection: 'row', width: 25, height: 25, borderRadius: 12, backgroundColor: 'transparent' }}>
-            <i className='far fa-bookmark' />
-          </button>
-        </div>
-      </div>
-    )
+      )
+    }
   }
 }
 
+// TODO(royhong): Make DRY
 class Main extends React.Component {
   render () {
     return (
       <div>
         <SearchBar />
-        <div className='is-flex' style={{ flexDirection: 'column-reverse' }}>
-          <div className='is-flex' style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
-            <div style={{ zIndex: 2, width: 56, height: 4, marginTop: -4, backgroundColor: '#228B22' }} />
-          </div>
-          <div className='is-flex' style={{ marginTop: -25, flexDirection: 'row', justifyContent: 'flex-end' }}>
-            <div style={{ width: 341, height: 100, backgroundColor: '#f2f2f2', borderRadius: 4 }}>
-              <div className='is-flex' style={{ width: '100%', height: '100%', justifyContent: 'space-between', alignItems: 'center' }}>
-                <p className='is-flex' style={{ zIndex: 1, width: '80%', alignItems: 'flex-end', fontSize: 26, fontWeight: 'bold', marginLeft: 14, textAlign: 'right', marginRight: 14 }}>
-                  BTS가 팬들에게 사주고 싶은 BLT
-                </p>
-                <p style={{ fontSize: 64, fontWeight: 'bold', marginRight: 14 }}>
-                  01
-                </p>
+        <div>
+          {/* Odd = Right Side */}
+          <div className='is-flex' style={{ flexDirection: 'column-reverse' }}>
+            <div className='is-flex' style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+              <div style={styles.greenBar} />
+            </div>
+            <div className='is-flex' style={styles.sandwichInfoOuterContainer}>
+              <div style={styles.sandwichInfoContainer}>
+                <div className='is-flex' style={styles.sandwichContainer}>
+                  <p className='is-flex' style={styles.sandwichTitle}>
+                    BTS가 팬들에게 사주고 싶은 BLT
+                  </p>
+                  <p style={{ fontSize: 64, fontWeight: 'bold', marginRight: 14 }}>
+                    01
+                  </p>
+                </div>
               </div>
             </div>
+            <div className='is-flex' style={{ paddingTop: 21, flexDirection: 'row', justifyContent: 'flex-end' }}>
+              <img src={images.rightSandwich} style={{ width: 326, height: 160 }} alt='sample_right' />
+            </div>
           </div>
-          <div className='is-flex' style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
-            <img src={images.rightSandwich} style={{ width: 326, height: 160 }} alt='sample_right' />
-          </div>
+          <StatusBar id={1} />
         </div>
-        <StatusBar />
+
+        <div>
+          {/* Even = Left Side */}
+          <div className='is-flex' style={{ flexDirection: 'column-reverse' }}>
+            <div className='is-flex' style={{ flexDirection: 'row', justifyContent: 'flex-start' }}>
+              <div style={{ zIndex: 2, width: 56, height: 4, marginTop: -4, backgroundColor: '#228B22' }} />
+            </div>
+            <div className='is-flex' style={{ marginTop: -25, flexDirection: 'row', justifyContent: 'flex-start' }}>
+              <div style={{ width: 341, height: 100, backgroundColor: COLORS.LIGHT_GREY, borderRadius: 4 }}>
+                <div className='is-flex' style={{ width: '100%', height: '100%', justifyContent: 'space-between', alignItems: 'center' }}>
+                  <p style={{ fontSize: 64, fontWeight: 'bold', marginRight: 14 }}>
+                    02
+                  </p>
+
+                  <p className='is-flex' style={{ zIndex: 1, width: '80%', alignItems: 'flex-end', fontSize: 26, fontWeight: 'bold', marginLeft: 14, textAlign: 'right', marginRight: 14 }}>
+                    불금 클럽 샌드위치
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className='is-flex' style={{ paddingTop: 21, flexDirection: 'row', justifyContent: 'flex-start' }}>
+              <img src={images.leftSandwich} style={{ width: 326, height: 160 }} alt='sample_right' />
+            </div>
+          </div>
+          <StatusBar id={2} />
+        </div>
       </div>
     )
   }

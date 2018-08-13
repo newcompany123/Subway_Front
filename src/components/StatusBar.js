163a0f6b966886f1a4cea6115427b1de531f5e39
Roy Hong <sroyhong313@gmail.com>
2018-08-13 17:00:58 +0900

    migrate statusBar to scss

--- src/components/StatusBar.js
+++ src/components/StatusBar.js
@@ -1,99 +1,32 @@
-import _ from 'lodash'
 import React from 'react'
 import PropTypes from 'prop-types'
 
 import { images } from '../common/ImageUtils'
-
-const styles = {
-  bookmarkContainer: {
-    paddingLeft: 10
-  },
-  bookmarkContents: {
-    flexDirection: 'row',
-    width: 25,
-    height: 25,
-    borderRadius: 12,
-    backgroundColor: 'transparent'
-  },
-  currentLoves: {
-    marginLeft: 3,
-    display: 'inline',
-    fontSize: 10
-  },
-  outerContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingTop: 7
-  },
-  loveContainer: {
-    width: 114,
-    height: 25,
-    borderRadius: 12,
-    backgroundColor: 'transparent',
-    justifyContent: 'center'
-  },
-  loveContainerEven: {
-    width: 114,
-    height: 25,
-    borderRadius: 12,
-    backgroundColor: 'transparent',
-    justifyContent: 'center',
-    marginLeft: 16
-  },
-  loveIcon: {
-    justifyContent: 'center'
-  },
-  shareContainer: {
-    paddingLeft: 7,
-    paddingRight: 17
-  },
-  shareContents: {
-    flexDirection: 'row',
-    width: 25,
-    height: 25,
-    borderRadius: 12,
-    backgroundColor: 'transparent'
-  }
-}
 
 export default class StatusBar extends React.PureComponent {
   static propTypes = {
     likes: PropTypes.number.isRequired
   };
 
-  // TODO(royhong): Switch second bookmark to export icon
   render () {
     const { likes } = this.props
 
-    const outerContainer = {}
-    const loveContainer = {}
-
-    if (this.props.id % 2 === 0) {
-      _.assign(outerContainer, styles.outerContainer, {justifyContent: 'flex-end'})
-      _.assign(loveContainer, styles.loveContainer)
-    } else {
-      _.assign(outerContainer, styles.outerContainer, {justifyContent: 'flex-start'})
-      _.assign(loveContainer, styles.loveContainer, { marginLeft: 16 })
-    }
+    const statusBar = this.props.id % 2 === 0
+      ? 'status-bar--right'
+      : 'status-bar--left'
 
     return (
-      <div className='is-flex' style={outerContainer}>
-        <div>
-          <button className='is-flex' style={loveContainer}>
-            <img src={images.love} style={styles.loveIcon} alt='love' />
-            <p style={styles.currentLoves}> {likes}명이 좋아해요</p>
+      <div className={statusBar}>
+          <button className='like-container'>
+            <img className='like-container__icon' src={images.love} alt='love' />
+            <p className='like-container__status'> {likes}명이 좋아해요</p>
           </button>
-        </div>
-        <div style={styles.bookmarkContainer}>
-          <button className='is-flex' style={styles.bookmarkContents}>
-            <img src={images.bookmark} alt='bookmark' />
+          <button className='bookmark-container'>
+            <img className='bookmark-container__icon' src={images.bookmark} alt='bookmark' />
           </button>
-        </div>
-        <div style={styles.shareContainer}>
-          <button className='is-flex' style={styles.shareContents}>
-            <img src={images.share} alt='share' width={14} height={14} />
+          <button className='share-container'>
+            <img src={images.share} alt='share' />
           </button>
-        </div>
       </div>
     )
   }

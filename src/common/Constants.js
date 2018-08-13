05a21f272f27e762ab445e0a2701ee467b98a46d
Roy Hong <sroyhong313@gmail.com>
2018-08-13 22:17:12 +0900

    WIP: refactor Ranking/Main Page

--- src/common/Constants.js
+++
@@ -1,8 +0,0 @@
-export const COLORS = {
-  SEA_BLUE: '#507cc0',
-  WARM_GREY: 'rgb(155, 155, 155)',
-  WHITE: '#ffffff',
-  YELLOW: '#ffcd00',
-  GREEN: '#228B22',
-  LIGHT_GREY: '#f2f2f2'
-}
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,50 +3,12 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import { connect } from 'react-redux'
 
-import { COLORS } from '../common/Constants'
 import { actionCreators } from '../models/actions/sandwich'
 
 import SearchBar from './SearchBar'
 import StatusBar from './StatusBar'
 
-// TODO(royhong): Clean up CSS, avoid inline styling
 // TODO(royhong): Make Contents responsive
-const styles = {
-  greenBar: {
-    width: 56,
-    height: 4,
-    marginTop: -4,
-    backgroundColor: COLORS.GREEN,
-    zIndex: 2
-  },
-  sandwichContainer: {
-    width: '100%',
-    height: '100%',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  sandwichInfoContainer: {
-    width: 341,
-    height: 100,
-    borderRadius: 4,
-    backgroundColor: COLORS.LIGHT_GREY
-  },
-  sandwichInfoOuterContainer: {
-    flexDirection: 'row',
-    marginTop: -25,
-    justifyContent: 'flex-end'
-  },
-  sandwichTitle: {
-    width: '80%',
-    alignItems: 'flex-end',
-    fontSize: 26,
-    fontWeight: 'bold',
-    marginLeft: 14,
-    textAlign: 'right',
-    marginRight: 14,
-    zIndex: 1
-  }
-}
 
 const initialState = {
   isLoading: false,
@@ -101,25 +63,23 @@ class Main extends React.Component {
   _renderRight (item, index) {
     return (
       <div key={index}>
-        <div className='is-flex' style={{ flexDirection: 'column-reverse' }}>
-          <div className='is-flex' style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
-            <div style={styles.greenBar} />
-          </div>
-          <div className='is-flex' style={styles.sandwichInfoOuterContainer}>
-            <div style={styles.sandwichInfoContainer}>
-              <div className='is-flex' style={styles.sandwichContainer}>
-                <p className='is-flex' style={styles.sandwichTitle}>
-                  { item.name.name }
-                </p>
-                <p style={{ fontSize: 64, fontWeight: 'bold', marginRight: 14 }}>
-                  { String(index + 1).padStart(2, '0') }
-                </p>
-              </div>
+        <div className='sandwich-item sandwich-item--right'>
+          <img src={item.sandwich.image_right} alt='sandwich_right' />
+        </div>
+        <div className='sandwich-item__details--right'>
+          <div className='sandwich-item__container sandwich-item__container--right'>
+            <div className='sandwich-item__container__name-ranking--right'>
+              <p className='sandwich-item__container__name-ranking sandwich-item__container__name-ranking__name--right'>
+                { item.name.name }
+              </p>
+              <p className='sandwich-item__container__name-ranking sandwich-item__container__name-ranking__ranking'>
+                { String(index + 1).padStart(2, '0') }
+              </p>
             </div>
           </div>
-          <div className='is-flex' style={{ paddingTop: 21, flexDirection: 'row', justifyContent: 'flex-end' }}>
-            <img src={item.sandwich.image_right} alt='sandwich_right' />
-          </div>
+        </div>
+        <div className='sandwich-item__divider sandwich-item__divider--right'>
+          <div className='sandwich-item__divider-green' />
         </div>
         <StatusBar id={index} likes={item.like_count} />
       </div>
@@ -129,25 +89,23 @@ class Main extends React.Component {
   _renderLeft (item, index) {
     return (
       <div key={index}>
-        <div className='is-flex' style={{ flexDirection: 'column-reverse' }}>
-          <div className='is-flex' style={{ flexDirection: 'row', justifyContent: 'flex-start' }}>
-            <div style={styles.greenBar} />
-          </div>
-          <div className='is-flex' style={{ marginTop: -25, flexDirection: 'row', justifyContent: 'flex-start' }}>
-            <div style={{ width: 341, height: 100, backgroundColor: COLORS.LIGHT_GREY, borderRadius: 4 }}>
-              <div className='is-flex' style={{ width: '100%', height: '100%', justifyContent: 'space-between', alignItems: 'center' }}>
-                <p style={{ fontSize: 64, fontWeight: 'bold', marginRight: 14 }}>
-                  { String(index + 1).padStart(2, '0') }
-                </p>
-                <p className='is-flex' style={{ zIndex: 1, width: '80%', alignItems: 'flex-end', fontSize: 26, fontWeight: 'bold', marginLeft: 14, textAlign: 'right', marginRight: 14 }}>
-                  { item.name.name }
-                </p>
-              </div>
+        <div className='sandwich-item sandwich-item--left'>
+          <img src={item.sandwich.image_left} alt='sandwich_left' />
+        </div>
+        <div className='sandwich-item__details--left'>
+          <div className='sandwich-item__container sandwich-item__container--left'>
+            <div className='sandwich-item__container__name-ranking--left'>
+              <p className='sandwich-item__container__name-ranking sandwich-item__container__name-ranking__name--left'>
+                { item.name.name }
+              </p>
+              <p className='sandwich-item__container__name-ranking sandwich-item__container__name-ranking__ranking'>
+                { String(index + 1).padStart(2, '0') }
+              </p>
             </div>
           </div>
-          <div className='is-flex' style={{ paddingTop: 21, flexDirection: 'row', justifyContent: 'flex-start' }}>
-            <img src={item.sandwich.image_left} height={160} width={320} alt='sample_left' />
-          </div>
+        </div>
+        <div className='sandwich-item__divider sandwich-item__divider--left'>
+          <div className='sandwich-item__divider-green' />
         </div>
         <StatusBar id={index} likes={item.like_count} />
       </div>

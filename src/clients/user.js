5c2d8ab06668c10309a608eb0a908a9c6f40f3a4
Roy Hong <sroyhong313@gmail.com>
2018-08-19 21:52:00 -0400

    render method and category buttons

--- src/clients/user.js
+++ src/clients/user.js
@@ -24,26 +24,3 @@ export default {
     })
   }
 }
-// const loginFacebookUser = (accessToken) => async dispatch => {
-//   try {
-//     const response = await axios.post(
-//       `${SERVER}/user/facebook-login/`,
-//       { 'access_token': accessToken }
-//     )
-//     dispatch({ type: LOGIN_USER, payload: response.data })
-//   } catch (error) {
-//     // TODO(royhong): handle error
-//   }
-// }
-
-// const loginKakaoUser = (accessToken) => async dispatch => {
-//   try {
-//     const response = await axios.post(
-//       `${SERVER}/user/kakao-login/`,
-//       { 'access_token': accessToken }
-//     )
-//     dispatch({ type: LOGIN_USER, payload: response.data })
-//   } catch (error) {
-//     // TODO(royhong): handle error
-//   }
-// }
--- src/components/FilterModal.js
+++ src/components/FilterModal.js
@@ -1,34 +1,83 @@
+import _ from 'lodash'
 import React from 'react'
 import Modal from 'react-modal'
 import { connect } from 'react-redux'
 
 import { actionCreators } from '../models/actions/screen'
 
+const METHOD = [
+  { type: '랭킹 높은 순' },
+  { type: '하트 많은 순' },
+  { type: '저장 많은 순' },
+  { type: '최신 등록 순' }
+]
+const CATEGORY = [
+  { status: '모두' },
+  { status: '신제품' },
+  { status: '프로모션' },
+  { status: '클래식' },
+  { status: '프래쉬 & 라이트' },
+  { status: '프리미엄' },
+  { status: '아침메뉴' }
+]
+
+function makeInitialState () {
+  return {
+    methodType: METHOD[0].type,
+    category: CATEGORY[0].status
+  }
+}
+
 class FilterModal extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = makeInitialState()
+  }
+
+  _changeMethod = (method) => {
+    this.setState({
+      methodType: method.type
+    })
+  }
+
+  _changeStatus = (category) => {
+    this.setState({
+      category: category.status
+    })
+  }
+
   render () {
+    const { filterModalVisible, closeFilterModal } = this.props
     return (
       <Modal
-        isOpen={this.props.filterModalVisible}
-        onRequestClose={this.props.closeFilterModal}
-        contentLabel='Example Modal'
+        isOpen={filterModalVisible}
+        onRequestClose={() => closeFilterModal() && this.setState(makeInitialState())}
       >
         <div className='filter-container'>
           <p className='filter-container__subtitle'>
             정렬 방식
           </p>
           <div className='method-container'>
-            <button className='method-container--method'>
-              <p className='method-container--method__text'>랭킹 높은 순</p>
-            </button>
-            <button className='method-container--method'>
-              <p className='method-container--method__text'>하트 많은 순</p>
-            </button>
-            <button className='method-container--method--selected'>
-              <p className='method-container--method__text--selected'>저장 많은 순</p>
-            </button>
-            <button className='method-container--method'>
-              <p className='method-container--method__text'>최신 등록 순</p>
-            </button>
+            { _.map(METHOD, method => {
+              let buttonStyle
+              let textStyle
+              if (method.type === this.state.methodType) {
+                buttonStyle = 'method-container--method--selected'
+                textStyle = 'method-container--method__text--selected'
+              } else {
+                buttonStyle = 'method-container--method'
+                textStyle = 'method-container--method__text'
+              }
+              return (
+                <button
+                  key={method.type}
+                  className={buttonStyle}
+                  onClick={() => this._changeMethod(method)}
+                >
+                  <p className={textStyle}>{method.type}</p>
+                </button>
+              )
+            }) }
           </div>
           <hr className='filter-container__border' />
           <p className='filter-container__subtitle'>
@@ -36,27 +85,26 @@ class FilterModal extends React.Component {
           </p>
           <div className='category-container'>
             <div className='category-container--content'>
-              <button className='category-container__button'>
-                <p className='category-container__button__text'>모두</p>
-              </button>
-              <button className='category-container__button'>
-                <p className='category-container__button__text'>신제품</p>
-              </button>
-              <button className='category-container__button'>
-                <p className='category-container__button__text'>프로모션</p>
-              </button>
-              <button className='category-container__button'>
-                <p className='category-container__button__text'>클래식</p>
-              </button>
-              <button className='category-container__button--selected'>
-                <p className='category-container__button__text--selected'>프래쉬 &amp; 라이트</p>
-              </button>
-              <button className='category-container__button'>
-                <p className='category-container__button__text'>프리미엄</p>
-              </button>
-              <button className='category-container__button'>
-                <p className='category-container__button__text'>아침메뉴</p>
-              </button>
+              { _.map(CATEGORY, category => {
+                let buttonStyle
+                let textStyle
+                if (category.status === this.state.category) {
+                  buttonStyle = 'category-container__button--selected'
+                  textStyle = 'category-container__button__text--selected'
+                } else {
+                  buttonStyle = 'category-container__button'
+                  textStyle = 'category-container__button__text'
+                }
+                return (
+                  <button
+                    key={category.status}
+                    className={buttonStyle}
+                    onClick={() => this._changeStatus(category)}
+                  >
+                    <p className={textStyle}>{category.status}</p>
+                  </button>
+                )
+              })}
             </div>
           </div>
           <hr className='category-container__border' />
